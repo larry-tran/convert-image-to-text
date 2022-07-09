472e44abbb44466a79b59c663ff53c4775f0be08
Trần Long <longtran4949@gmail.com>
2022-07-09 23:03:11 +0700

    upgrade angular

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const cors = require("cors");
 const express = require("express");
+const favicon = require("serve-favicon");
+const path = require("path");
 const uploadRoute = require("./routes/uploadRoute");
 const toolRoute = require("./routes/toolRoute");
 
+const router = express.Router();
+
 const dotenv = require("dotenv");
+const publicRoute = require("./routes/publicRoute");
 dotenv.config();
 
 const app = express();
@@ -17,8 +22,21 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(favicon(__dirname + "/public/www/favicon.ico"));
 
 app.use("/api", uploadRoute);
 app.use("/tool", toolRoute);
 
 app.listen(PORT, () => console.log(`Server is ready at ${PORT}`));
+
+router.use(
+    express.static(
+      path.join(
+        __dirname,
+        process.env["base-dir"] ? process.env["base-dir"] : "./public/www"
+      )
+    )
+  );
+  
+app.use("/", router);
+module.exports = app;

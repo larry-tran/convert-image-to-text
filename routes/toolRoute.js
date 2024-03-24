d497f3312ee7d64bba74d0ddc202fe3155d9fbb0
LARRYTRAN\longt <longtran4949@gmail.com>
2024-03-24 13:21:34 +0700

    change package

--- routes/toolRoute.js
+++ routes/toolRoute.js
@@ -1,22 +1,9 @@
 const router = require("express").Router();
 const path = require("path");
-const noteModel = require ("../models/note.model")
 const appDir = path.dirname(require.main.filename);
 
 router.get("/imagetotext", (req, res) => {
   res.sendFile(path.join(appDir, "pages/ConvertImageToText.html"));
-});
-
-// router.get("/mergeparagraph", (req, res) => {
-//   res.sendFile(path.join(appDir, "pages/MergeParagraph.html"));
-// });
-
-router.get("notes", (req, res) => {
-  noteModel.find().then(data=>{
-      res.json(data);
-  }).catch(err=>{
-      console.log(err);
-  })
 });
 
 module.exports = router;
